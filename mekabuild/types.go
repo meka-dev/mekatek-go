68c991a40a05985fd78ede73324613fe90c08e86
Tomás Senart <tsenart@gmail.com>
2022-09-05 19:32:14 +0200

    Sign bytes use txsHash

--- mekabuild/types.go
+++ mekabuild/types.go
@@ -17,7 +17,7 @@ type Signer interface {
 // BuildBlockRequest represents a request from a validator to the build endpoint
 // of the builder API. In order to meet the pattern used by other signable types
 // in Tendermint, it contains a Signature field that needs to be set by callers.
-// See SignableBytes for more detail.
+// See SignBytes for more detail.
 type BuildBlockRequest struct {
 	ChainID          string   `json:"chain_id"`
 	Height           int64    `json:"height"`
@@ -29,18 +29,9 @@ type BuildBlockRequest struct {
 	Signature []byte `json:"signature"`
 }
 
-// SignableBytes returns a stable byte representation of the signable fields of
-// a BuildBlockRequest. After constructing a complete value, callers should
-// invoke this method, sign the returned bytes with the private key that
-// corresponds to the validator address, and set the Signature field to that
-// signature.
-func (req *BuildBlockRequest) SignableBytes() []byte {
-	return BuildBlockRequestSignableBytes(req.ChainID, req.Height, req.ValidatorAddress, req.MaxBytes, req.MaxGas, req.Txs)
-}
-
-// BuildBlockRequestSignableBytes returns a stable byte representation of a
+// BuildBlockRequestSignBytes returns a stable byte representation of a
 // BuildBlockRequest represented by the provided parameters.
-func BuildBlockRequestSignableBytes(chainID string, height int64, validatorAddr string, maxBytes, maxGas int64, txs [][]byte) []byte {
+func BuildBlockRequestSignBytes(chainID string, height int64, validatorAddr string, maxBytes, maxGas int64, txsHash []byte) []byte {
 	// XXX: Changing the order or the set of fields that are signed will cause
 	// verification failures unless both the signer and verifier are updated.
 	// Tread carefully.
@@ -58,11 +49,8 @@ func BuildBlockRequestSignableBytes(chainID string, height int64, validatorAddr 
 	mustEncode(&sb, []byte(validatorAddr))
 	mustEncode(&sb, maxBytes)
 	mustEncode(&sb, maxGas)
-	mustEncode(&sb, uint64(len(txs)))
-	for _, tx := range txs {
-		mustEncode(&sb, uint64(len(tx)))
-		mustEncode(&sb, tx)
-	}
+	mustEncode(&sb, uint64(len(txsHash)))
+	mustEncode(&sb, txsHash)
 	return sb.Bytes()
 }
 
@@ -79,23 +67,15 @@ type BuildBlockResponse struct {
 // RegisterChallenge is used by the builder API during registration to establish
 // the identity of the registering validator. In order to meet the pattern used
 // by other signable types in Tendermint, it contains a Signature field that
-// needs to be set by callers. See SignableBytes for more detail.
+// needs to be set by callers. See SignBytes for more detail.
 type RegisterChallenge struct {
 	Bytes     []byte `json:"bytes"`
 	Signature []byte `json:"signature,omitempty"`
 }
 
-// SignableBytes returns a stable byte representation of the challenge. After
-// constructing a complete value, callers should invoke this method, sign the
-// returned bytes with the private key that corresponds to the validator address
-// provided during registration, and set the Signature field to that signature.
-func (rc *RegisterChallenge) SignableBytes() []byte {
-	return RegisterChallengeSignableBytes(rc.Bytes)
-}
-
-// RegisterChallengeSignableBytes returns a stable byte representation of the
+// RegisterChallengeSignBytes returns a stable byte representation of the
 // RegisterChallenge represented by the provided parameters.
-func RegisterChallengeSignableBytes(ch []byte) []byte {
+func RegisterChallengeSignBytes(chainID string, challenge []byte) []byte {
 	// XXX: Changing the order or the set of fields that are signed will cause
 	// verification failures unless both the signer and verifier are updated.
 	// Tread carefully.
@@ -106,8 +86,10 @@ func RegisterChallengeSignableBytes(ch []byte) []byte {
 
 	var sb bytes.Buffer
 	mustEncode(&sb, []byte(`register-challenge`))
-	mustEncode(&sb, uint64(len(ch)))
-	mustEncode(&sb, ch)
+	mustEncode(&sb, uint64(len(chainID)))
+	mustEncode(&sb, []byte(chainID))
+	mustEncode(&sb, uint64(len(challenge)))
+	mustEncode(&sb, challenge)
 	return sb.Bytes()
 }
 
