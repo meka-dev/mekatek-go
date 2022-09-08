3dcde1b60b15b564ad12a80ff0b96cc32321bd62
0000 1000 1101 0010 <fffe@08d2.com>
2022-09-08 17:16:30 +0200

    Documentation of decompression middleware

--- mekabuild/httputil.go
+++ mekabuild/httputil.go
@@ -2,16 +2,23 @@ package mekabuild
 
 import (
 	"compress/gzip"
+	"fmt"
 	"net/http"
 	"strings"
 )
 
+// GunzipRequestMiddleware inspects the Content-Encoding header of the incoming
+// request. If it specifies a supported compression scheme i.e. gzip, the body
+// will be wrapped with a decompressor i.e. gzip.Reader.
 func GunzipRequestMiddleware(h http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		if strings.Contains(r.Header.Get("content-encoding"), "gzip") {
-			if zr, err := gzip.NewReader(r.Body); err == nil {
-				r.Body = zr
+			zr, err := gzip.NewReader(r.Body)
+			if err != nil {
+				http.Error(w, fmt.Errorf("gzip reader: %w", err).Error(), http.StatusBadRequest)
+				return
 			}
+			r.Body = zr
 		}
 		h.ServeHTTP(w, r)
 	})
